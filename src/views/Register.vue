afa2dd005d9641dd023d905d9522b10237d30ef1
waserende <916040853@qq.com>
2021-06-15 11:26:05 +0800

    封装axios请求

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -26,7 +26,7 @@
               <el-form-item label="手机号">
                 <el-input placeholder="请输入手机号"
                           clearable
-                          v-model.number="ruleForm.telephone"></el-input>
+                          v-model="ruleForm.telephone"></el-input>
               </el-form-item>
               <el-form-item label="密码">
                 <el-input placeholder="请输入密码"
@@ -49,7 +49,10 @@
 
 <script>
 // import { ref } from 'vue';
-import axios from 'axios';
+
+import { ElMessage } from 'element-plus';
+import storageService from '@/service/storageService';
+import userService from '@/service/userService';
 
 export default ({
   // setup() {
@@ -59,11 +62,12 @@ export default ({
   //     password: ref(''),
   //   };
   // },
+
   data() {
     return {
       ruleForm: {
         name: '',
-        teltphone: '',
+        telephone: '',
         password: '',
       },
       rules: {
@@ -85,14 +89,20 @@ export default ({
     };
   },
   methods: {
+
     register() {
       console.log(this.ruleForm);
-      axios.post('http://localhost:1016/register', { ...this.ruleForm }).then((res) => {
+      // 请求
+      userService.register(this.ruleForm).then((res) => {
         // 保存token
         console.log(res.data);
+        storageService.set(storageService.USER_TOKEN, res.data.data.token);
+
         // 跳转主页
+        this.$router.replace({ name: 'Home' });
       }).catch((err) => {
         console.log('err', err.response.data.msg);
+        ElMessage.error(String(err.response.data.msg));
       });
       console.log('register');
     },
